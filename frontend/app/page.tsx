19ebebb69f160fec824c88644b4031c3c2c83524
Signe <signexmarie@hotmail.com>
2023-05-18 13:14:37 +0200

    modals, input fields, forms

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import TopNav from '../src/components/TopNav'
+import Modal from '../src/components/Modal'
+import EventForm from '../src/components/EventForm'
 
 export default async function Home() {
   const sites = ['My site', 'My other site', 'My third site']
@@ -8,6 +10,7 @@ export default async function Home() {
   return (
     <main >
       <TopNav sites={sites} navElements={navElements} />
+      <Modal title={"Create an event"} buttonText={"Create event"} children={<EventForm />} />
     </main>
   );
 }
