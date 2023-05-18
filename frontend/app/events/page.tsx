381b4c6eee4026ec36a1253287ddf754f88a333f
Niklas Vejlby Schjoldager <61137786+niklasschjoldager@users.noreply.github.com>
2023-05-18 16:30:16 +0200

    Correct layout in events

--- frontend/app/events/page.tsx
+++ frontend/app/events/page.tsx
@@ -1,7 +1,5 @@
 import TopNav from "@/src/components/TopNav";
 import EventsOverview from "@/src/components/EventsOverview";
-import Modal from "@/src/components/Modal";
-import EventForm from "@/src/components/EventForm";
 
 export default async function Events() {
   const sites = ["My site", "My other site", "My third site"];
@@ -10,10 +8,6 @@ export default async function Events() {
   return (
     <main className="max-w-screen-lg mx-auto mt-[137px] px-4 py-[100px]">
       <TopNav sites={sites} navElements={navElements} />
-      <EventsOverview />
-      <Modal title={"Create an event"} buttonText={"Create event"}>
-        <EventForm />
-      </Modal>
       <EventsOverview />
     </main>
   );
