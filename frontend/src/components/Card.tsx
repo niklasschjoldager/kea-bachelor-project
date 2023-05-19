68996beb9490e89bc804f4bc963b0940cd6a4b9d
linearesights <linea@resights.dk>
2023-05-19 17:26:44 +0200

    calendar half done

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const Card = ({ children }: Props) => {
     return (
-        <div className="bg-white border-[1px] rounded-2 border-card-border shadow-card p-6 flex flex-col gap-5">
+        <div className="bg-white border-[1px] rounded-2 border-card-border shadow-card p-6 flex flex-col gap-6">
             {children}
         </div>
     )
--- frontend/src/components/EventsOverview.tsx
+++ frontend/src/components/EventsOverview.tsx
@@ -2,13 +2,15 @@ import EventsList from './EventsList'
 import ViewSwitcher from './ViewSwitcher'
 import Modal from './Modal'
 import EventForm from './EventForm'
+import Calender from './calendar/Calendar'
+
 
 const EventsOverview = () => {
     const events = [
         {
             'title': 'Vinfestival',
             'short_desc': 'Så slår vi igen pavillonerne op i vores gårdhave og inviterer nogle af vores producenter herop. Kom og mød de dejlige mennesker, der laver vinene, og smag på varerne.',
-            'date': 'Fredag 12 April',
+            'date': '12/04/2023',
             'time': '16:00 - 23:00',
             'address': 'Prins Valdemars Alle 5C, 3450 Allerød',
             'image_path': 'image_1',
@@ -17,7 +19,7 @@ const EventsOverview = () => {
         {
             'title': 'Wine & Dine på Espehus Kro',
             'short_desc': 'Vi gentager succesen fra sidste år og teamer endnu engang op med Espehus Kro for en mindeværdig wine & dine aften, denne gang med vine fra både Di Meo og Marion.',
-            'date': 'Fredag 12 April',
+            'date': '12/04/2023',
             'time': '16:00 - 23:00',
             'address': 'Prins Valdemars Alle 5C, 3450 Allerød',
             'image_path': 'image_2'
@@ -26,7 +28,7 @@ const EventsOverview = () => {
         {
             'title': 'Kældersmagning deluxe',
             'short_desc': 'Så er der igen linet op til en af de rigtig gode aftener. Her kommer de store og sjældne vine frem fra kælderen. Og vi har et par spændende nyheder med her også...',
-            'date': 'Fredag 12 April',
+            'date': '04/07/2023',
             'time': '16:00 - 23:00',
             'address': 'Prins Valdemars Alle 5C, 3450 Allerød',
             'image_path': 'image_3'
@@ -35,13 +37,23 @@ const EventsOverview = () => {
         {
             'title': 'Vinfestival',
             'short_desc': 'Så slår vi igen pavillonerne op i vores gårdhave og inviterer nogle af vores producenter herop. Kom og mød de dejlige mennesker, der laver vinene, og smag på varerne.',
-            'date': 'Fredag 12 April',
+            'date': '20/06/2023',
+            'time': '16:00 - 23:00',
+            'address': 'Prins Valdemars Alle 5C, 3450 Allerød',
+            'image_path': 'image_1'
+
+        },
+        {
+            'title': 'Vinfestival',
+            'short_desc': 'Så slår vi igen pavillonerne op i vores gårdhave og inviterer nogle af vores producenter herop. Kom og mød de dejlige mennesker, der laver vinene, og smag på varerne.',
+            'date': '19/05/2023',
             'time': '16:00 - 23:00',
             'address': 'Prins Valdemars Alle 5C, 3450 Allerød',
             'image_path': 'image_1'
 
         },
     ]
+
 
     const views = [{text: 'List', icon: 'list', position: 'left'}, {text: 'Calendar', icon: 'calendar', position: 'right'}]
     return (
@@ -54,7 +66,8 @@ const EventsOverview = () => {
                 <h2 className='text-h2 text-slate-gray leading-none'>May</h2>
                 <ViewSwitcher views={views}/>
             </div>
-            <EventsList events={events} />
+            {/* <EventsList events={events} /> */}
+            <Calender events={events}/>
         </>
     )
 }
