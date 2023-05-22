376a839e64c6fc50e9f3ac8151686882689ab9dd
Niklas Vejlby Schjoldager <61137786+niklasschjoldager@users.noreply.github.com>
2023-05-22 15:05:49 +0200

    fix swagger docs login

--- frontend/app/api/auth/[...nextauth]/route.ts
+++ frontend/app/api/auth/[...nextauth]/route.ts
@@ -11,7 +11,7 @@ async function login(credentials: { username: string; password: string }) {
     })
     .join("&");
 
-  const res = await fetch("http://127.0.0.1:8000/login", {
+  const res = await fetch("http://127.0.0.1:8000/token", {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     body: body,
@@ -33,7 +33,9 @@ async function signup(credentials: {
 }) {
   const res = await fetch("http://127.0.0.1:8000/signup", {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify(credentials),
   });
 
