3c34909dabd862e4c10d64f57eac7754c0c25aed
Niklas Vejlby Schjoldager <61137786+niklasschjoldager@users.noreply.github.com>
2023-05-24 14:08:13 +0200

    Events & integration page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,28 +3,19 @@ import { getServerSession } from "next-auth/next";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import SignInForm from "@/components/SignInForm";
 import Link from "next/link";
-import TopNav from "@/components/TopNav";
-import EventsOverview from "@/components/EventsOverview";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
-  if (!session) {
-    return (
-      <main className="flex flex-col items-center justify-center min-h-screen p-24">
-        <h1 className="text-4xl">Log in</h1>
-        <SignInForm />
-        <Link href="/signup">Create account</Link>
-      </main>
-    );
+  if (session) {
+    redirect("/events");
   }
 
   return (
-    <main>
-      <TopNav />
-      <div className="mx-auto max-w-screen-lg px-4 py-[100px]">
-        <EventsOverview />
-      </div>
+    <main className="flex flex-col items-center justify-center min-h-screen p-24">
+      <h1 className="text-4xl">Log in</h1>
+      <SignInForm />
+      <Link href="/signup">Create account</Link>
     </main>
   );
 }
