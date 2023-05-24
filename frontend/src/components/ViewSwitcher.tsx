134eed8aac6f884e74c980dcb82571011ec17096
linearesights <linea@resights.dk>
2023-05-24 10:53:58 +0200

    view switcher changes

--- frontend/src/components/ViewSwitcher.tsx
+++ frontend/src/components/ViewSwitcher.tsx
@@ -12,18 +12,23 @@ type Props = {
 };
 
 const ViewSwitcher = ({ views }: Props) => {
+  const [value, setValue] = React.useState('List');
   return (
     <ToggleGroup.Root
       className="inline-flex overflow-hidden transition border rounded-2 border-card-border"
       type="single"
-      defaultValue="left"
       aria-label="Text alignment"
+      value={value}
+      onValueChange={(value) => {
+        if (value) setValue(value);
+        console.log(value, 'switcher value')
+      }}
     >
       {views.map((view, index) => (
         <div key={index} className="flex cursor-pointer">
           <ToggleGroup.Item
             className="inline-flex grow items-center gap-2 px-3 py-2 transition data-[state=on]:pointer-events-none data-[state=on]:bg-white data-[state=on]:shadow-switcher"
-            value={view.position}
+            value={view.text}
           >
             <Image
               priority
