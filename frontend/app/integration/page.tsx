3e6075e732c91d6cb8aaa6c24db59dffdd66dd0a
Signe <signexmarie@hotmail.com>
2023-05-26 10:31:39 +0200

    integrations page

--- frontend/app/integration/page.tsx
+++ frontend/app/integration/page.tsx
@@ -1,4 +1,5 @@
 import TopNav from "@/components/common/TopNav";
+import IntegrationsOverview from "@/components/views/IntegrationsOverview";
 
 export default async function Integration() {
 
@@ -8,7 +9,6 @@ export default async function Integration() {
       <div className="mx-auto max-w-screen-lg px-4 py-[100px]">
         <h1 className="text-h1">Integration page</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-
       </div>
     </main>
   );
--- frontend/src/components/views/IntegrationsOverview.tsx
+++ frontend/src/components/views/IntegrationsOverview.tsx
@@ -1,5 +1,35 @@
-const IntegrationsOverview = () => {
+import Card from "../ui/Card";
 
+const IntegrationsOverview = () => {
+    const integrationStep = [
+        {
+            step: "1",
+            title: "Title",
+            subtitle: "Subtitle",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            code: "hello"
+        },
+        {
+            step: "2",
+            title: "Title",
+            subtitle: "Subtitle",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            code: "hello"
+        }
+    ]
+    return (
+        integrationStep.map(step => {
+            return (
+                <Card key={step.step}>
+                    <div>
+                        {step.step}
+
+                    </div>
+                </Card>
+            )
+
+        })
+    )
 }
 
 export default IntegrationsOverview;
