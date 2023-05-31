4931214e67a27c29140b8f9dfbec76aa3828bdae
Niklas Vejlby Schjoldager <61137786+niklasschjoldager@users.noreply.github.com>
2023-05-31 11:14:23 +0200

    integration guide styling

--- frontend/src/components/ui/CodeBlock.tsx
+++ frontend/src/components/ui/CodeBlock.tsx
@@ -24,10 +24,10 @@ const CodeBlock = ({ children }: Props) => {
   };
 
   return (
-    <div className="relative bg-ghost-white px-3 pb-3 pt-11">
-      <pre className="overflow-x-auto ">{children}</pre>
+    <div className="relative py-3 pl-3 bg-ghost-white pr-11">
+      <pre className="overflow-x-auto text-label">{children}</pre>
       <div
-        className="absolute right-0 top-0 flex h-8 w-8 cursor-pointer items-center justify-center bg-slate-gray transition hover:bg-paynes-gray"
+        className="absolute top-0 right-0 flex items-center justify-center w-8 h-8 transition cursor-pointer bg-slate-gray hover:bg-paynes-gray"
         onClick={(event) => copy(event)}
       >
         <Image
@@ -35,12 +35,12 @@ const CodeBlock = ({ children }: Props) => {
           src={CopyIcon}
           alt="Copy icon"
           width={20}
-          className="pointer-events-none transition"
+          className="transition pointer-events-none"
         />
         <div
           className={`${
             showTooltip ? "opacity-100" : "opacity-0"
-          } rounded-sm pointer-events-none absolute top-10 bg-dark-gray px-2 py-1 text-white transition-opacity after:absolute after:-top-1 after:left-1/2 after:h-2 after:w-2 after:-translate-x-1/2 after:rotate-45 after:bg-dark-gray`}
+          } pointer-events-none absolute top-10 rounded-sm bg-dark-gray px-2 py-1 text-white transition-opacity after:absolute after:-top-1 after:left-1/2 after:h-2 after:w-2 after:-translate-x-1/2 after:rotate-45 after:bg-dark-gray`}
         >
           Copied!
         </div>
