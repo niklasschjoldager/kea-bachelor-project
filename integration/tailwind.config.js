7ff08cbd945c00e5b8dad82f9da43456b8de3238
Niklas Vejlby Schjoldager <61137786+niklasschjoldager@users.noreply.github.com>
2023-05-31 10:42:21 +0200

    Remove wrong animation

--- integration/tailwind.config.js
+++ integration/tailwind.config.js
@@ -53,11 +53,6 @@ export default {
       to: { opacity: 1, transform: "translate(-50%, -50%) scale(1)" },
     },
   },
-  animation: {
-    slideUpAndFade: "slideUpAndFade .2s cubic-bezier(0.16, 1, 0.3, 1)",
-    overlayShow: "overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
-    contentShow: "contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
-  },
   extend: {},
   plugins: [],
 };
