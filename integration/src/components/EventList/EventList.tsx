b1e91af535b18333e971dcff0b918f70e7774821
Niklas Vejlby Schjoldager <61137786+niklasschjoldager@users.noreply.github.com>
2023-06-02 09:40:47 +0200

    Event lit space

--- integration/src/components/EventList/EventList.tsx
+++ integration/src/components/EventList/EventList.tsx
@@ -8,11 +8,11 @@ function EventList({ events }: EventListProps) {
   console.log(events);
 
   return events && events.length > 0 ? (
-    <>
+    <div className="grid gap-6">
       {events.map((event) => (
         <Event key={event.id} {...event} />
       ))}
-    </>
+    </div>
   ) : (
     <p>No events</p>
   );
